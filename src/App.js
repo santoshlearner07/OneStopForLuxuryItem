b0e741e01ee6e25596b4996acf5c88394bd2b56d
Santosh Nandiyawar <nandiyawarsantosh.0719@gmail.com>
2024-06-21 14:52:34 +0530

    Updated:-View on map features

--- src/App.js
+++ src/App.js
@@ -5,15 +5,44 @@ import FooterBar from "./fixedComponent/FooterBar"
 import SearchBar from "./component/SearchBar"
 import Product from "./component/Product"
 import { useEffect, useState } from 'react';
-import axios from 'axios';
-
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
+
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const response = await fetch('http://localhost:8888/api/properties');
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setProperties(data.properties);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchProperties();
+  }, []);
+
   return (
     <Container className="App" fluid="xxl">
       <HeaderBar />
       <SearchBar />
-      <Product />
+      <Routes>
+    <Route path='/' element={
+      <Product properties={properties} loading={loading} errMsg={error} />
+    } />
+      </Routes>
       <FooterBar />
     </Container>
   );
--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,57 +1,84 @@
 import React, { useEffect, useState } from 'react'
+import { Card, Col, Container, ListGroup, Row } from 'react-bootstrap';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useNavigate, useParams } from 'react-router-dom';
 
 function Product(props) {
-  const [properties, setProperties] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchProperties = async () => {
-      try {
-        const response = await fetch('http://localhost:8888/api/properties');
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        setProperties(data.properties);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-        setLoading(false);
-      }
-    };
-
-    fetchProperties();
-  }, []);
-
-  if (loading) {
+  const [show, hide] = useState(null)
+  const navigate = useNavigate();
+  const zoom = 13;
+  if (props.loading) {
     return <div>Loading...</div>;
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
+  if (props.errMsg) {
+    return <div>Error: {props.errMsg.message}</div>;
   }
-  console.log(properties)
+  console.log(props.properties)
+
+  // const viewOnMap = (mapId) => {
+  //   navigate(`/${mapId}`)
+  // }
+  const viewOnMap = (mapId) => {
+    hide(show === mapId ? null : mapId);
+  }
+
   return (
-    <div>
-      <h1>Properties</h1>
-      <ol>
-        {properties.map((item, index) => {
-          return <div key={index}>
-            <li>
-              id:- {item.id}
-              ----Summary:- {item.summary}
-              <br />
-              <h1>Price:- {item.price.amount}</h1>
-              <br />
-              <img src={item.customer.brandPlusLogoUrl} />
-            </li>
-          </div>
-        })}
-      </ol>
-    </div>
+    <Container>
+      <h1>One Stop For Luxury Item</h1>
+      <Row>
+        <Col sm={3} style={{ backgroundColor: "aqua" }}> 1</Col>
+        <Col>
+          <Row xl={2} lg={2} md={1} sm={1} xs={1} >
+            {props.properties.map((item, index) => {
+              return (
+                <Col key={index}>
+                  <Card>
+                    <Card.Img variant="top" src={item.propertyImages.mainMapImageSrc} />
+                    <Card.Body>
+                      <Card.Title>Displayed:- {item.addedOrReduced}</Card.Title>
+                      <Card.Text>
+                        id:- {item.id}
+                      </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                      <ListGroup.Item>Bathrooms:- {item.bathrooms} and Bedrooms:- {item.bedrooms}</ListGroup.Item>
+                      <ListGroup.Item>Address:- {item.displayAddress}</ListGroup.Item>
+                      <ListGroup.Item>Price:- {item.price.amount} £</ListGroup.Item>
+                      <ListGroup.Item>Brief:- {item.propertyTypeFullDescription}</ListGroup.Item>
+                      <ListGroup.Item onClick={() => viewOnMap(item.id)}>View On Map</ListGroup.Item>
+                      <ListGroup.Item>
+                        {show === item.id && (
+                          // <MapContainer center={[item.location.latitude, item.location.longitude]} zoom={13} scrollWheelZoom={false}>
+                          //   <TileLayer
+                          //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                          //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                          //   />
+                          //   <Marker position={[item.location.latitude, item.location.longitude]}>
+                          //     <Popup>
+                          //       A pretty CSS3 popup. <br /> Easily customizable.
+                          //     </Popup>
+                          //   </Marker>
+                          // </MapContainer>
+                          <iframe
+                            width={"375"}
+                            height={"300"}
+                            style={{ border: "none" }}
+                            src={`https://maps.google.com/maps?q=${item.location.latitude},${item.location.longitude}&z=${zoom}&output=embed`}
+                            title="google map"
+                          />
+                        )}
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card>
+                  <br />
+                </Col>
+              )
+            })}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
